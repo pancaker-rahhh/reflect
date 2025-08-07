c8b061f0a83e77a8bac388f29753211764f43100
keshavrathinavel <keshavrathinavel@gmail.com>
2025-08-07 17:38:15 +0530

    Modify root docker compose to use alpine base image

--- api/server/migrations/env.py
+++ api/server/migrations/env.py
@@ -1,12 +1,9 @@
 import asyncio
 from logging.config import fileConfig
-
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
 from alembic import context
-
 import sys
 from pathlib import Path
 
