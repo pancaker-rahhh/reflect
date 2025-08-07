09deb90946c697382e8be76cf5e12ac776cbbeb0
keshavrathinavel <keshavrathinavel@gmail.com>
2025-08-07 17:30:09 +0530

    Simplify health response

--- api/server/app/schemas/health.py
+++ api/server/app/schemas/health.py
@@ -2,15 +2,16 @@ from pydantic import BaseModel, Field
 
 
 class HealthCheckResponse(BaseModel):
-    status: str = Field(..., description="Health status of the service", example="healthy")
-    service: str = Field(..., description="Name of the service", example="Reflect API")
-    version: str = Field(..., description="Version of the service", example="0.1.0")
-    
+    status: str = Field(...)
+    service: str = Field(...)
+    version: str = Field(...)
+
     class Config:
         json_schema_extra = {
-            "example": {
-                "status": "healthy",
-                "service": "Reflect API",
-                "version": "0.1.0"
-            }
+            'description': 'Provides health status information about the service.',
+            'example': {
+                'status': 'healthy',
+                'service': 'Reflect API',
+                'version': '0.1.0',
+            },
         }
