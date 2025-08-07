03bfba073b8f40d3762367cd8560d5d27d42ee99
ShafiudeenKameel <skameel2020@gmail.com>
2025-08-07 22:22:52 +0530

    build project backend api calls

--- api/server/app/router/api_router.py
+++ api/server/app/router/api_router.py
@@ -1,17 +1,11 @@
 from fastapi import APIRouter
 
-from app.router.v1 import  project_router, health_router, workspace_router
+from app.router.v1 import project_router, health_router, workspace_router
 
-api_router = APIRouter(prefix="/api/v1")
+api_router = APIRouter(prefix='/api/v1')
 
 api_router.include_router(health_router.health_router)
+api_router.include_router(project_router.router, prefix='/projects', tags=['Projects'])
 api_router.include_router(
-    project_router.router,
-    prefix="/projects",
-    tags=["Projects"]
-)
-api_router.include_router(
-    workspace_router.router,
-    prefix="/workspaces",
-    tags=["Workspaces"]
+    workspace_router.router, prefix='/workspaces', tags=['Workspaces']
 )
