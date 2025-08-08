e50b1d732d40d59659458404e3ac5bb2b091d7ac
ShafiudeenKameel <skameel2020@gmail.com>
2025-08-08 20:45:23 +0530

    modify widget files to use real api instead of mock

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,23 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AppProvider } from '@/context/AppContext'
 
-async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return
-  }
-
-  const { worker } = await import('./mocks/browser')
-  
-  return worker.start({
-    onUnhandledRequest: 'bypass',
-  })
-}
-
-enableMocking().then(() => {
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AppProvider>
       <App />
-    </StrictMode>,
-  )
-})
+    </AppProvider>
+  </StrictMode>
+)
