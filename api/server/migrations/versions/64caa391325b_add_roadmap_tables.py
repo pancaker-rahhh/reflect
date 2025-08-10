0945517b9c7e597041d39d97a2def171450b1583
Indira Kumar A K <indirakumarak@gmail.com>
2025-08-10 16:18:19 +0530

    re-order broken migration files, give names for empty ones

--- api/server/migrations/versions/64caa391325b_add_roadmap_tables.py
+++ api/server/migrations/versions/64caa391325b_add_roadmap_tables.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '64caa391325b'
-down_revision: Union[str, Sequence[str], None] = '86cdee4025b8'
+down_revision: Union[str, Sequence[str], None] = 'f62f34ac6069'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
