6e5a0b414565b8d7ad8fbf0f0f500a48902983fa
ShafiudeenKameel <skameel2020@gmail.com>
2025-08-20 20:15:19 +0530

    add logos

---
+++ reflect-landing-page/src/app/components/logos.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+export const NextjsLogo = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg {...props} viewBox="0 0 128 128" width="128" height="128">
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M64 128C99.3462 128 128 99.3462 128 64C128 28.6538 99.3462 0 64 0C28.6538 0 0 28.6538 0 64C0 99.3462 28.6538 128 64 128ZM39.1365 111.459C39.1365 111.459 27.5912 98.599 26.3262 96.9038C25.1381 95.3045 25.1381 93.6092 26.3262 92.0099L73.1633 34.4163L50.4021 34.4163L50.4021 34.4163L24.781 34.4163L24.781 29H82.4925H103.219V34.4163H82.4925L39.1365 87.724L65.5292 87.724L83.8569 64.9189L88.2255 70.1504C88.2255 70.1504 100.283 83.4756 101.548 85.1708C102.736 86.7701 102.736 88.4654 101.548 90.0647L54.7109 147.658L77.4721 147.658L77.4721 147.658L103.093 147.658L103.093 153.075H45.3815H24.6548V147.658H45.3815L88.7375 94.3499L62.3448 94.3499L39.1365 111.459Z"
+            fill="black"
+        />
+    </svg>
+);
+
+export const ReactLogo = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg {...props} viewBox="-11.5 -10.23174 23 20.46348">
+        <circle cx="0" cy="0" r="2.05" fill="#61dafb" />
+        <g stroke="#61dafb" strokeWidth="1" fill="none">
+            <ellipse rx="11" ry="4.2" />
+            <ellipse rx="11" ry="4.2" transform="rotate(60)" />
+            <ellipse rx="11" ry="4.2" transform="rotate(120)" />
+        </g>
+    </svg>
+);
+
+export const VueLogo = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg {...props} viewBox="0 0 256 221">
+        <path fill="#42b883" d="M204.8 0H256L128 220.8L0 0h97.92L128 51.2L157.44 0h47.36z" />
+        <path fill="#35495e" d="M0 0l128 220.8L256 0h-51.2L128 132.48L50.56 0H0z" />
+    </svg>
+);
+
+export const WordpressLogo = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg {...props} viewBox="0 0 1000 1000">
+        <path d="M500,1000C223.9,1000,0,776.1,0,500S223.9,0,500,0s500,223.9,500,500S776.1,1000,500,1000z M278.8,791.7l101-270.8L278.8,250h442.4l-101,270.8l101,270.8H278.8z M500,708.3l69.8,187.5h156.9L500,343.8L273.3,895.8h156.9L500,708.3z M187.5,500c0,172.9,140.4,313.3,312.5,313.3S812.5,672.9,812.5,500S672.1,186.7,500,186.7S187.5,327.1,187.5,500z" />
+    </svg>
+);
+
+export const LaravelLogo = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg {...props} viewBox="0 0 50 50">
+        <path d="M25,2C12.318,2,2,12.318,2,25s10.318,23,23,23s23-10.318,23-23S37.682,2,25,2z M39.311,35.311l-7,4l-7-4v-8l7,4l7-4V35.311z M25.311,25.311l-7,4l-7-4v-8l7,4l7-4V25.311z M32.311,17.311l-7,4l-7-4v-8l7,4l7-4V17.311z" />
+    </svg>
+);
+
+export const DjangoLogo = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg {...props} viewBox="0 0 118 64">
+        <path fill="#092E20" d="M0 0h118v64H0z" />
+        <path fill="#FFF" d="M14 14h18v12h-6v18h-12V14zm24 0h12v30h18v-30h12v36h-42V14zm52 0h20v6h-8v30h-12V14zm-4-10a4 4 0 1 1 0 8 4 4 0 0 1 0-8z" />
+    </svg>
+);
+
+export const WebflowLogo = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg {...props} viewBox="0 0 24 24">
+        <path d="M11.25 0h1.5v24h-1.5z" />
+        <path d="M12 11.25H0v1.5h24v-1.5z" />
+    </svg>
+);
+
+export const FramerLogo = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg {...props} viewBox="0 0 24 24">
+        <path d="M0 0h24v24H0z" fill="none" />
+        <path d="M4 4h16v2H4zM4 10h16v2H4zM4 16h16v2H4z" />
+    </svg>
+);
+
+export const ShopifyLogo = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg {...props} viewBox="0 0 24 24">
+        <path d="M19.49 5.41A9.15 9.15 0 0012 2.5a9.15 9.15 0 00-7.49 2.91A8.91 8.91 0 002.5 12a8.91 8.91 0 002.01 5.59A9.15 9.15 0 0012 21.5a9.15 9.15 0 007.49-2.91A8.91 8.91 0 0021.5 12a8.91 8.91 0 00-2.01-5.59zM12 20a7.5 7.5 0 01-7.07-4.59.5.5 0 01.45-.69h13.24a.5.5 0 01.45.69A7.5 7.5 0 0112 20zm-5-7.5a.5.5 0 01-.5-.5V10a.5.5 0 01.5-.5h2a.5.5 0 01.5.5v2a.5.5 0 01-.5.5zm8 0a.5.5 0 01-.5-.5V10a.5.5 0 01.5-.5h2a.5.5 0 01.5.5v2a.5.5 0 01-.5.5z" />
+    </svg>
+);
