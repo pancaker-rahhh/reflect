d502e692bfa1f3009ff83b8ffb5d0051db842b61
Indira Kumar A K <74497486+Indira-kumar@users.noreply.github.com>
2025-09-09 23:46:45 +0530

    remove reload from prod app, change health back to /health endpoint

--- api/server/run.py
+++ api/server/run.py
@@ -7,7 +7,12 @@ load_dotenv()
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', '8000'))
+    debug = os.getenv('DEBUG', 'false').lower() == 'true'
 
     uvicorn.run(
-        'app.main:app', host='0.0.0.0', port=port, reload=True, log_level='info'
+        'app.main:app', 
+        host='0.0.0.0', 
+        port=port, 
+        reload=debug,  # Only reload in debug mode
+        log_level='info'
     )
