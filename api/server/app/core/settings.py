47d136914dc7c870b91c629ce93a3064660bcd9a
Indira Kumar A K <74497486+Indira-kumar@users.noreply.github.com>
2025-09-09 20:11:36 +0530

    convert db_url secret to string instead of postgres DSN

--- api/server/app/core/settings.py
+++ api/server/app/core/settings.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 from typing import List, Optional
-from pydantic import field_validator, PostgresDsn
+from pydantic import field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
     POSTGRES_PASSWORD: str = 'reflect_dev_pass'
     POSTGRES_HOST: str = 'localhost'
     POSTGRES_PORT: int = 5432
-    DATABASE_URL: Optional[PostgresDsn] = None
+    DATABASE_URL: str = None
     DATABASE_POOL_SIZE: int = 20
     DATABASE_MAX_OVERFLOW: int = 0
 
