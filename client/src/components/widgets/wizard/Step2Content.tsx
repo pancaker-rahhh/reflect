23b16e6b3ad213fdc577ffb2d7c4ff1caa8aa6e4
ShafiudeenKameel <skameel2020@gmail.com>
2025-10-02 10:49:06 +0530

    fix build errors

--- client/src/components/widgets/wizard/Step2Content.tsx
+++ client/src/components/widgets/wizard/Step2Content.tsx
@@ -11,7 +11,6 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
-import { Switch } from '@/components/ui/switch'
 import { type WidgetFormData } from '@/pages/WidgetCreate'
 import {
   Select,
