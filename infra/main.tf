6677a4259510b801e12133dcf7659f97a538f638
keshavrathinavel <keshavrathinavel@gmail.com>
2025-10-17 23:06:33 +0530

    Configure terraform state bucket

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,16 @@
 terraform {
+  required_version = ">= 1.6.0"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
+  }
+
+  backend "gcs" {
+    bucket = "reflectfeedback-terraform-state"
+    prefix = "cloud-run"
   }
 }
 
