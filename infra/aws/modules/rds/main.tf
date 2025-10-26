dff2ee9d366071c23a30ebbcae103d3e7ab9a2c6
Keshav Rathinavel <73035121+keshavrathinavel@users.noreply.github.com>
2025-10-26 19:13:08 +0530

    Add route table association and fix directory to cd into on workflow (#213)

--- infra/aws/modules/rds/main.tf
+++ infra/aws/modules/rds/main.tf
@@ -39,6 +39,29 @@ resource "aws_internet_gateway" "main" {
   )
 }
 
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = merge(
+    {
+      Name        = "${var.identifier}-rt"
+      Environment = var.environment
+    },
+    var.tags
+  )
+}
+
+resource "aws_route_table_association" "subnet" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.main.id
+}
+
 resource "aws_db_subnet_group" "main" {
   name       = "${var.identifier}-subnet-group"
   subnet_ids = aws_subnet.private[*].id
@@ -50,6 +73,10 @@ resource "aws_db_subnet_group" "main" {
     },
     var.tags
   )
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_security_group" "rds" {
